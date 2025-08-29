52f29ab76e3f507e7652120149eeb84966951b46
Joseph Ditton <jditton.atomic@gmail.com>
2025-08-29 09:38:53 -0600

    first commit

---
+++ FirstJava/src/MyProgram.java
@@ -0,0 +1,19 @@
+class MyProgram {
+    public static void main(String[] args) {
+        boolean isAdmin = true; // false 1 byte
+        byte num1 = 100;
+        short num2 = 300; // 2 bytes
+        char myChar = 'a'; // 2 bytes
+        int num3 = 234987; // 4 bytes
+        float num5 = 234234.8f; // 4 bytes
+        long num4 = 324523498; // 8 bytes
+        double num6 = 23423423423.34; // 8 bytes
+
+
+        short myShort = num3;
+
+
+
+    }
+
+}
